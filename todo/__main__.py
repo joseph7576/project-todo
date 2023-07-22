ec6ff3792b6446636050f45c111685018a7d49fe
joe <joeho7576@gmail.com>
2023-07-22 21:00:17 +0330

    finish building the app through toturial

--- todo/__main__.py
+++ todo/__main__.py
@@ -1,1 +1,10 @@
-# Provides an entry-point script to run the app from the package using the python -m todo command
+"""To-Do entry point script."""
+
+from todo import cli, __app_name__
+
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+if __name__ == "__main__":
+    main()
